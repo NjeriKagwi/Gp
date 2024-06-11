8e2dc9607c2b3ed9b327237d1456c8dc086fc95f
NjeriKagwi <141217917+NjeriKagwi@users.noreply.github.com>
2024-06-11 16:11:24 +0300

    Add files via upload

---
+++ Gp/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", () => {
+    const scrollY = window.pageYOffset;
+  
+    if(scrollY > 5){
+      document.querySelector("nav").classList.add("navbar-scrolled");
+    }else{
+      document.querySelector("nav").classList.remove("navbar-scrolled");
+    }
+})
+
+
+
+  
